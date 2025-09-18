127777e8ff3b1ed2e0f6f7499f89cdd60e71c569
Manuel Schenkel <manuel.schenkel@gmail.com>
2025-09-18 16:19:05 +0200

    replace single with douple quotes

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,7 +9,7 @@ export async function fetchManagementGroups() {
       // console.log('Fetching revenue data...');
       // await new Promise((resolve) => setTimeout(resolve, 3000));
   
-      const data = await sql`SELECT * FROM 'management_groups';`;
+      const data = await sql`SELECT * FROM "management_groups";`;
   
       // console.log('Data fetch completed after 3 seconds.');
   
