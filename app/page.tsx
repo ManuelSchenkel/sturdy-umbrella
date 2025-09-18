3a85c048efaf5a706480ed74b739344f9bcbfc55
Manuel Schenkel <manuel.schenkel@gmail.com>
2025-09-18 15:06:26 +0200

    fetch mgmt groups

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
 import { unstable_noStore as noStore}  from 'next/cache';
+import { fetchManagementGroups } from './lib/data';
 
-export default function Home() {
+export default async function Home() {
   noStore();
+
+  const managementGroups = await fetchManagementGroups();
+  console.log(managementGroups);
 
   const timeOnServer = new Date().toLocaleTimeString('en-US');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         Hybrid Rendering - Server Time {timeOnServer}
+      </div>
+      <div>
+
       </div>
     </main>
   );
