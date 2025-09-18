1ca9d11b8f45900671058a84d23c11f89ec2d039
Manuel Schenkel <manuel.schenkel@gmail.com>
2025-09-18 16:17:50 +0200

    update query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,13 +9,13 @@ export async function fetchManagementGroups() {
       // console.log('Fetching revenue data...');
       // await new Promise((resolve) => setTimeout(resolve, 3000));
   
-      const data = await sql`SELECT * FROM management_groups`;
+      const data = await sql`SELECT * FROM 'management_groups';`;
   
       // console.log('Data fetch completed after 3 seconds.');
   
       return data;
     } catch (error) {
       console.error('Database Error:', error);
-      throw new Error('Failed to fetch revenue data.');
+      throw new Error('Failed to fetch management groups');
     }
   }
