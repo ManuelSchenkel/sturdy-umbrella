65976da30ad9935b2d7f1d1d4cbcd86d9d05cd96
Manuel Schenkel <manuel.schenkel@gmail.com>
2025-09-18 15:15:40 +0200

    log stuufffff

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,14 @@ import { fetchManagementGroups } from './lib/data';
 export default async function Home() {
   noStore();
 
-  const managementGroups = await fetchManagementGroups();
-  console.log(managementGroups);
+  //const managementGroups = await fetchManagementGroups();
+  //console.log(managementGroups);
+
+  console.log(process.env.DB_HOST);
+  console.log(process.env.DB_PORT);
+  console.log(process.env.DB_DATABASE);
+  console.log(process.env.DB_USER);
+  console.log(process.env.DB_PASSWORD);
 
   const timeOnServer = new Date().toLocaleTimeString('en-US');
   return (
