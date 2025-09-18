dbf531442d2c51cabbbc81c2aa68579a2bcdc9ce
Manuel Schenkel <manuel.schenkel@gmail.com>
2025-09-18 15:33:15 +0200

    add db output

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,15 @@ export default async function Home() {
   //const managementGroups = await fetchManagementGroups();
   //console.log(managementGroups);
 
-  console.log("hellooo");
-  console.log(process.env.DB_HOST);
-  console.log(process.env.DB_PORT);
-  console.log(process.env.DB_DATABASE);
-  console.log(process.env.DB_USER);
-  console.log(process.env.DB_PASSWORD);
-
   const timeOnServer = new Date().toLocaleTimeString('en-US');
   console.log("hellooo2");
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         Hybrid Rendering - Server Time {timeOnServer}
+      </div>
+      <div>
+        DB: {process.env.DB_DATABASE}
       </div>
     </main>
   );
